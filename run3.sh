80a8b3df24e59f850352b851ec13873939fe1451
shamnastv <shamnastv@gmail.com>
2020-08-19 00:10:10 +0530

    update

--- run3.sh
+++ run3.sh
@@ -3,6 +3,6 @@
 #SBATCH --ntasks=1 # Run on a single CPU
 #SBATCH --time=11:50:00 # Time limit hrs:min:sec
 #SBATCH --output=test_job%j.out # Standard output and error log
-#SBATCH --gres=gpu:6
-#SBATCH --partition=q2h_12h-2G
+#SBATCH --gres=gpu:1
+#SBATCH --partition=q2h_12h-32C
 python3 main1.py --epochs 1000 --lr .000001 --num_mlp_layers 4 --hidden_dim 768 --final_dropout .5 --graph_pooling_type average --neighbor_pooling_type average --learn_eps --configfile param.yaml --alpha 100
