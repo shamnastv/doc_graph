27360b407c5c501d12156b14e548a288af9dfb03
shamnastv <shamnastv@gmail.com>
2020-08-18 08:58:59 +0530

    update

--- run2.sh
+++ run2.sh
@@ -5,4 +5,4 @@
 #SBATCH --output=test_job%j.out # Standard output and error log
 #SBATCH --gres=gpu:1
 #SBATCH --partition=q2h_12h-32C
-python3 main1.py --epochs 1000 --lr .00001 --num_mlp_layers 4 --hidden_dim 768 --final_dropout .5 --graph_pooling_type average --neighbor_pooling_type average --learn_eps --configfile param.yaml --alpha 100 --device 1
+python3 main1.py --epochs 1000 --lr .000001 --num_mlp_layers 5 --hidden_dim 768 --final_dropout .5 --graph_pooling_type average --neighbor_pooling_type average --learn_eps --configfile param.yaml --alpha 100
