ece504093f77460ba487b7463d32361bd0e06e44
shamnastv <shamnastv@gmail.com>
2020-08-25 15:14:18 +0530

    update

--- main_mod.py
+++ main_mod.py
@@ -269,7 +269,7 @@ def main():
 
     for epoch in range(10):
         avg_loss, ge = train(args, model_e, model_c, device, graphs, optimizer, optimizer_c, epoch,
-                                 train_size, ge, initial=Ture)
+                                 train_size, ge, initial=True)
     acc_train, acc_test, ge_new = test(args, model_e, model_c, device, graphs, train_size, epoch, ge)
 
     ge = ge_new
