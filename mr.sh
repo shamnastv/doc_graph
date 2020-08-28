4e2fbd5f2fdfd65aeae39d3abe7d874ccb136b29
shamnastv <shamnastv@gmail.com>
2020-08-28 21:26:05 +0530

    update

--- mr.sh
+++ mr.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #SBATCH --job-name=GNN # Job name
-#SBATCH --ntasks=1 # Run on a single CPU
-#SBATCH --time=11:50:00 # Time limit hrs:min:sec
+#SBATCH --ntasks=4 # Run on a single CPU
+#SBATCH --time=23:50:00 # Time limit hrs:min:sec
 #SBATCH --output=test_job%j.out # Standard output and error log
 #SBATCH --gres=gpu:1
-#SBATCH --partition=q2h_12h-32C
-python3 main.py --epochs 300 --lr .0001 --num_mlp_layers 3 --hidden_dim 768 --final_dropout .2 --graph_pooling_type average --neighbor_pooling_type average --learn_eps --configfile config/mr.yaml --alpha .5
+#SBATCH --partition=q2h_24h-2G
+python3 main_mod.py --batch_size_cl 500 --epochs 1000 --iters_per_epoch 100 --lr .00001 --lr_c .1 --num_layers 5 --num_mlp_layers 3 --num_mlp_layers_c 3 --hidden_dim 768 --final_dropout .5 --graph_pooling_type average --neighbor_pooling_type sum --configfile config/ohsumed.yaml --alpha 1000
