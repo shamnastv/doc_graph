6febdf2f74b172ef28ccca797e9a459fb3cebe86
Shamnas <shamnastv@gmail.com>
2020-09-04 23:14:23 +0530

    update

--- mr.sh
+++ mr.sh
@@ -4,5 +4,5 @@
 #SBATCH --time=23:50:00 # Time limit hrs:min:sec
 #SBATCH --output=test_job%j.out # Standard output and error log
 #SBATCH --gres=gpu:1
-#SBATCH --partition=cl1_48h-1G
-python3 main.py --iters_per_epoch 60 --epochs 1000 --lr .0001 --lr_c .01 --num_mlp_layers 5 --num_mlp_layers_c 3 --hidden_dim 768 --final_dropout .5 --graph_pooling_type average --neighbor_pooling_type average --learn_eps --configfile config/mr.yaml --alpha 1000
+#SBATCH --partition=cl2_48h-1G
+python3 main.py --batch_size 32 --iters_per_epoch 120 --update_freq 1000 --epochs 3000 --lr .0001 --lr_c .01 --num_mlp_layers 5 --num_mlp_layers_c 3 --hidden_dim 768 --final_dropout .5 --graph_pooling_type average --neighbor_pooling_type average --learn_eps --configfile config/mr.yaml --alpha 1500
