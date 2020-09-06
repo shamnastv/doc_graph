02c9a2004eba692dfe6758610debc4ff530b517f
Shamnas <shamnastv@gmail.com>
2020-09-06 14:27:01 +0530

    update

--- main_mod.py
+++ main_mod.py
@@ -333,10 +333,10 @@ def main():
     print('Embedding Initialized', flush=True)
     # acc_train, acc_test, ge_new = test(args, model_e, model_c, device, graphs, train_size, 10, ge)
 
-    # for i in range(len(ge)):
-    #     norm = ge_new[i].norm(p=2, dim=1, keepdim=True)
-    #     ge[i] = ge_new[i].div(norm)
-    ge = ge_new
+    for i in range(len(ge)):
+        norm = ge_new[i].norm(p=2, dim=1, keepdim=True)
+        ge[i] = ge_new[i].div(norm)
+    # ge = ge_new
 
     for epoch in range(1, args.epochs + 1):
         avg_loss, ge_new = train(args, model_e, model_c, device, graphs, optimizer, optimizer_c, epoch, train_size, ge)
@@ -344,10 +344,10 @@ def main():
         scheduler.step()
 
         if epoch % args.iters_per_epoch == 0:
-            # for i in range(len(ge)):
-            #     norm = ge_new[i].norm(p=2, dim=1, keepdim=True)
-            #     ge[i] = ge_new[i].div(norm)
-            ge = ge_new
+            for i in range(len(ge)):
+                norm = ge_new[i].norm(p=2, dim=1, keepdim=True)
+                ge[i] = ge_new[i].div(norm)
+            # ge = ge_new
 
             # model_c = ClusterNN(num_classes, graphs[0].node_features.shape[1], args.hidden_dim, args.num_layers,
             #                     args.num_mlp_layers_c).to(device)
