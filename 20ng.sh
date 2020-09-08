0d1ab0d71bc3dcebf995f2944e0d195b0d0b442e
Shamnas <shamnastv@gmail.com>
2020-09-08 08:57:24 +0530

    update

--- 20ng.sh
+++ 20ng.sh
@@ -4,5 +4,5 @@
 #SBATCH --time=23:50:00 # Time limit hrs:min:sec
 #SBATCH --output=test_job%j.out # Standard output and error log
 #SBATCH --gres=gpu:1
-#SBATCH --partition=cl2_48h-1G
+#SBATCH --partition=cl1_48h-1G
 python3 main.py --batch_size 32 --iters_per_epoch 120 --update_freq 500 --epochs 2000 --lr .00001 --lr_c .01 --num_mlp_layers 3 --num_mlp_layers_c 3 --hidden_dim 768 --final_dropout .5 --graph_pooling_type average --neighbor_pooling_type average --learn_eps --configfile config/20ng.yaml --alpha 1000
