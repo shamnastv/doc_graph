943f5151b7804f52cc10b1d97a376d7e5f1fa16e
Shamnas <shamnastv@gmail.com>
2020-09-22 21:04:28 +0530

    update

--- main_mod.py
+++ main_mod.py
@@ -36,9 +36,9 @@ class S2VGraph(object):
         self.g = g
         self.node_tags = node_tags
         # self.neighbors = []
-        node_features = torch.FloatTensor(node_features)
-        norm = torch.norm(node_features, p=2, dim=1, keepdim=True)
-        self.node_features = node_features.div(norm)
+        self.node_features = torch.FloatTensor(node_features)
+        # norm = torch.norm(node_features, p=2, dim=1, keepdim=True)
+        # self.node_features = node_features.div(norm)
         self.edge_mat = 0
         self.edges_weights = []
 
