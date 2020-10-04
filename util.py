9debf51394cfed18777d1c78b758ca0b34c0ce31
Shamnas <shamnastv@gmail.com>
2020-10-04 22:19:38 +0530

    update

--- util.py
+++ util.py
@@ -1,6 +1,7 @@
 import yaml
 import scipy.sparse as sp
 import numpy as np
+import re
 
 
 def read_param(filename):
@@ -17,3 +18,45 @@ def normalize_adj(adj):
     d_inv_sqrt[np.isinf(d_inv_sqrt)] = 0.
     d_mat_inv_sqrt = sp.diags(d_inv_sqrt)
     return adj.dot(d_mat_inv_sqrt).transpose().dot(d_mat_inv_sqrt).tocoo()
+
+
+def loadWord2Vec(filename):
+    """Read Word Vectors"""
+    vocab = []
+    embd = []
+    word_vector_map = {}
+    file = open(filename, 'r')
+    for line in file.readlines():
+        row = line.strip().split(' ')
+        if len(row) > 2:
+            vocab.append(row[0])
+            vector = row[1:]
+            length = len(vector)
+            for i in range(length):
+                vector[i] = float(vector[i])
+            embd.append(vector)
+            word_vector_map[row[0]] = vector
+    print('Loaded Word Vectors!')
+    file.close()
+    return vocab, embd, word_vector_map
+
+
+def clean_str(string):
+    """
+    Tokenization/string cleaning for all datasets except for SST.
+    Original taken from https://github.com/yoonkim/CNN_sentence/blob/master/process_data.py
+    """
+    string = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", string)
+    string = re.sub(r"\'s", " \'s", string)
+    string = re.sub(r"\'ve", " \'ve", string)
+    string = re.sub(r"n\'t", " n\'t", string)
+    string = re.sub(r"\'re", " \'re", string)
+    string = re.sub(r"\'d", " \'d", string)
+    string = re.sub(r"\'ll", " \'ll", string)
+    string = re.sub(r",", " , ", string)
+    string = re.sub(r"!", " ! ", string)
+    string = re.sub(r"\(", " \( ", string)
+    string = re.sub(r"\)", " \) ", string)
+    string = re.sub(r"\?", " \? ", string)
+    string = re.sub(r"\s{2,}", " ", string)
+    return string.strip().lower()
