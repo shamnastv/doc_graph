78cb39cf255b6abd48a63bb4061221640870db47
Shamnas <shamnastv@gmail.com>
2020-10-05 17:02:23 +0530

    update

--- main_mod.py
+++ main_mod.py
@@ -15,7 +15,7 @@ from gnn_mod import GNN
 from util import normalize_adj, row_norm
 
 criterion = nn.CrossEntropyLoss()
-frequency_as_feature = False
+frequency_as_feature = True
 max_val_accuracy = 0
 test_accuracy = 0
 max_acc_epoch = 0
@@ -37,8 +37,7 @@ class S2VGraph(object):
         self.node_tags = node_tags
         # self.neighbors = []
         self.node_features = torch.FloatTensor(node_features)
-        # norm = torch.norm(node_features, p=2, dim=1, keepdim=True)
-        self.node_features = row_norm(self.node_features)
+        # self.node_features = row_norm(self.node_features)
         self.edge_mat = 0
         self.edges_weights = []
 
@@ -355,10 +354,9 @@ def main():
     print('Embedding Initialized', flush=True)
     # acc_train, acc_test, ge_new = test(args, model_e, model_c, device, graphs, train_size, 10, ge)
 
-    for i in range(len(ge)):
-        ge[i] = row_norm(ge_new[i])
-        # ge[i] = torch.from_numpy(normalize(ge_new[i].cpu())).to(device)
-    # ge = ge_new
+    # for i in range(len(ge)):
+    #     ge[i] = row_norm(ge_new[i])
+    ge = ge_new
 
     for epoch in range(1, args.epochs + 1):
         avg_loss, ge_new, cl = train(args, model_e, model_c, device, graphs, optimizer,
@@ -367,10 +365,9 @@ def main():
         scheduler.step()
 
         if epoch % args.iters_per_epoch == 0 or True:
-            for i in range(len(ge)):
-                ge[i] = row_norm(ge_new[i])
-            #     ge[i] = torch.from_numpy(normalize(ge_new[i].cpu())).to(device)
-            # ge = ge_new
+            # for i in range(len(ge)):
+            #     ge[i] = row_norm(ge_new[i])
+            ge = ge_new
 
             # model_c = ClusterNN(num_classes, graphs[0].node_features.shape[1], args.hidden_dim, args.num_layers,
             #                     args.num_mlp_layers_c).to(device)
