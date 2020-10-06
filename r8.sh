1bf1778cfb2e68ec7a09e2a312eb9b21396e5ea9
Shamnas <shamnastv@gmail.com>
2020-10-06 18:40:16 +0530

    update

--- r8.sh
+++ r8.sh
@@ -5,4 +5,4 @@
 #SBATCH --output=test_job%j.out # Standard output and error log
 #SBATCH --gres=gpu:1
 #SBATCH --partition=cl1_48h-1G
-python3 main_mod.py --epochs 200 --lr .0001 --num_layers 4 --num_mlp_layers 1 --hidden_dim 300 --final_dropout .5 --graph_pooling_type average --neighbor_pooling_type sum --learn_eps --configfile config/R8.yaml
+python3 main_mod.py --epochs 200 --lr .0001 --num_layers 4 --num_mlp_layers 1 --hidden_dim 200 --final_dropout .5 --graph_pooling_type average --neighbor_pooling_type sum --learn_eps --configfile config/R8.yaml
