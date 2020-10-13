7e88e2bf48ba11671eb43a6263a82163248b5f72
Shamnas <shamnastv@gmail.com>
2020-10-13 13:30:03 +0530

    update

--- r8.sh
+++ r8.sh
@@ -5,4 +5,4 @@
 #SBATCH --output=test_job%j.out # Standard output and error log
 #SBATCH --gres=gpu:1
 #SBATCH --partition=cl1_48h-1G
-python3 main_mod.py --epochs 300 --iters_per_epoch 100 --lr .00001 --lr_c .01 --num_layers 3 --num_mlp_layers 2 --num_mlp_layers_c 3 --hidden_dim 400 --final_dropout .5 --graph_pooling_type average --neighbor_pooling_type sum --learn_eps --configfile config/R8.yaml --alpha 500
+python3 main_mod.py --epochs 300 --iters_per_epoch 100 --lr .00001 --lr_c .01 --num_layers 3 --num_mlp_layers 2 --num_mlp_layers_c 2 --hidden_dim 400 --final_dropout .5 --graph_pooling_type average --neighbor_pooling_type sum --learn_eps --configfile config/R8.yaml --alpha 500
--- run3.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#SBATCH --job-name=GNN # Job name
-#SBATCH --ntasks=4 # Run on a single CPU
-#SBATCH --time=23:50:00 # Time limit hrs:min:sec
-#SBATCH --output=test_job%j.out # Standard output and error log
-#SBATCH --gres=gpu:1
-#SBATCH --partition=cl1_48h-1G
-python3 main_mod.py --epochs 500 --iters_per_epoch 100 --lr .00005 --lr_c .01 --num_layers 5 --num_mlp_layers 1 --num_mlp_layers_c 3 --hidden_dim 768 --final_dropout .5 --graph_pooling_type average --neighbor_pooling_type sum --learn_eps --configfile param2.yaml --alpha 1000
