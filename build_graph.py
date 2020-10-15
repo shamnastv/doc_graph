4cc9d883661e513430d37eda4cb00899e7ec06ad
Shamnas <shamnastv@gmail.com>
2020-10-15 18:37:01 +0530

    update

--- build_graph.py
+++ build_graph.py
@@ -274,7 +274,7 @@ def build_graph(config_file='param.yaml'):
             count = word_pair_count[key]
             word_freq_i = word_window_freq[vocab[i]]
             word_freq_j = word_window_freq[vocab[j]]
-            pmi = log((40.0 * count / num_window) /
+            pmi = log((10.0 * count / num_window) /
                       (1.0 * word_freq_i * word_freq_j / (num_window * num_window)))
             if pmi <= 0:
                 print('dropped edge : ', vocab[i], ' ', vocab[j], ' ', exp(pmi))
