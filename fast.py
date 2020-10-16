4cd95c89447ff2c1e3631d131d05a6868f7dad85
Shamnas <shamnastv@gmail.com>
2020-10-16 22:36:56 +0530

    update

--- fast.py
+++ fast.py
@@ -1,6 +1,6 @@
-import fast
+import fasttext
 
-model = fast.train_unsupervised('data/corpus/R8.clean.txt')
+model = fasttext.train_unsupervised('data/corpus/R8.clean.txt')
 
 model.save_model("model")
 
