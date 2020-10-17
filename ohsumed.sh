23f1b046b387ac6cf1ae17c5b43b4a9ee812929f
Shamnas <shamnastv@gmail.com>
2020-10-17 13:07:03 +0530

    update

--- ohsumed.sh
+++ ohsumed.sh
@@ -5,4 +5,4 @@
 #SBATCH --output=test_job%j.out # Standard output and error log
 #SBATCH --gres=gpu:1
 #SBATCH --partition=cl1_48h-1G
-python3 main.py --epochs 400 --iters_per_epoch 50 --lr .0001 --lr_c .01 --num_layers 3 --num_mlp_layers 2 --num_mlp_layers_c 3 --hidden_dim 300 --final_dropout .5 --graph_pooling_type average --neighbor_pooling_type sum --learn_eps --configfile ohsumed --alpha 1000
+python3 main.py --epochs 400 --iters_per_epoch 50 --lr .0001 --lr_c .01 --num_layers 3 --num_mlp_layers 2 --num_mlp_layers_c 3 --hidden_dim 300 --final_dropout .5 --graph_pooling_type average --neighbor_pooling_type sum --learn_eps --configfile ohsumed --alpha 1000 --k_fold 0 --early_stop 80
