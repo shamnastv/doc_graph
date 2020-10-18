bc5c08ceafd1a99eac8b889fa5d094ef7aa3ad8e
Shamnas <shamnastv@gmail.com>
2020-10-18 14:45:58 +0530

    update

--- fast.py
+++ fast.py
@@ -1,9 +1,12 @@
 import fasttext
 
-model = fasttext.train_unsupervised('data/corpus/R8.clean.txt', dim=300)
+dataset = 'wiki'
+# model = fasttext.train_unsupervised('data/corpus/R8.clean.txt', dim=300)
+model = fasttext.train_unsupervised('data/corpus/' + dataset + '.clean.txt', dim=400)
 
 model.save_model("model")
 
+print('model saved')
 # model = fasttext.load_model("model")
-x = model.get_word_vector("the")
+# x = model.get_word_vector("the")
 
