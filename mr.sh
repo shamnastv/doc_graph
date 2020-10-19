8a6ea83a5dc2947a777049dfc5485a2dc2832b15
Shamnas <shamnastv@gmail.com>
2020-10-19 09:23:18 +0530

    update

--- mr.sh
+++ mr.sh
@@ -5,4 +5,4 @@
 #SBATCH --output=test_job%j.out # Standard output and error log
 #SBATCH --gres=gpu:1
 #SBATCH --partition=cl2_48h-1G
-python3 main.py --epochs 400 --iters_per_epoch 50 --lr .00004 --lr_c .01 --num_layers 3 --num_mlp_layers 2 --num_mlp_layers_c 2 --hidden_dim 400 --final_dropout .5 --graph_pooling_type average --neighbor_pooling_type sum --learn_eps --configfile mr
+python3 main.py --epochs 400 --iters_per_epoch 50 --lr .00004 --lr_c .01 --num_layers 3 --num_mlp_layers 1 --num_mlp_layers_c 2 --hidden_dim 400 --final_dropout .5 --graph_pooling_type average --neighbor_pooling_type sum --learn_eps --configfile mr
