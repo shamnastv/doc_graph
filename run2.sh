da9c82d025a1295c59290bbcee355bffa347a161
Shamnas <shamnastv@gmail.com>
2020-12-26 15:39:26 +0530

    update

--- run2.sh
+++ run2.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-#SBATCH --job-name=GNN # Job name
+#SBATCH --job-name=mr # Job name
 #SBATCH --ntasks=4 # Run on a single CPU
 #SBATCH --time=23:50:00 # Time limit hrs:min:sec
-#SBATCH --output=test_job%j.out # Standard output and error log
+#SBATCH --output=test_job%j%x.out # Standard output and error log
 #SBATCH --gres=gpu:1
 #SBATCH --partition=cl1_48h-1G
-python3 main.py --epochs 400 --lr .00008 --num_layers 3 --num_mlp_layers 2 --hidden_dim 400 --final_dropout .5 --graph_pooling_type average --neighbor_pooling_type sum --learn_eps --configfile param2
+python3 main.py --lr .001 --num_layers 4 --num_mlp_layers 1 --hidden_dim 100 --final_dropout .5 --learn_eps --configfile mr
