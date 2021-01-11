218d63a283142cc6edff63b11ec368aeecff47e1
Shamnas <shamnastv@gmail.com>
2021-01-11 23:31:02 +0530

    update

--- layer.py
+++ layer.py
@@ -31,8 +31,8 @@ class GNNLayer(nn.Module):
             x_cat = [features[idx[0]], features[idx[1]], elem.unsqueeze(1)]
             x_cat = torch.cat(x_cat, dim=1)
 
-            # elem_new1 = F.leaky_relu(self.edge_wt[head](x_cat).squeeze(1), negative_slope=0.2)
-            elem_new1 = self.edge_wt[head](x_cat) / torch.tensor(15.0).to(self.device)
+            elem_new1 = F.leaky_relu(self.edge_wt[head](x_cat).squeeze(1), negative_slope=0.2)
+            # elem_new1 = self.edge_wt[head](x_cat) / torch.tensor(15.0).to(self.device)
             with torch.no_grad():
                 maximum = torch.max(elem_new1)
             elem_new = elem_new1 - maximum
