e3d54d90e0e96fdb4d9a34f9f73f85d781c58d8b
Shamnas <shamnastv@gmail.com>
2021-01-11 22:41:08 +0530

    update

--- r8.sh
+++ r8.sh
@@ -6,5 +6,5 @@
 #SBATCH --gres=gpu:1
 #SBATCH --partition=cl1_48h-1G
 
-python3 main.py --epochs 400 --lr .0001 --num_layers 3 --num_mlp_layers 1 --hidden_dim 200 --final_dropout .5 --configfile R8_save
-python3 main.py --epochs 400 --lr .00008 --num_layers 3 --num_mlp_layers 1 --hidden_dim 200 --final_dropout .5 --configfile R8_save
+python3 main.py --epochs 400 --lr .0001 --num_layers 3 --num_mlp_layers 1 --hidden_dim 200 --final_dropout .5 --configfile R8_save --num_heads 2
+python3 main.py --epochs 400 --lr .00008 --num_layers 3 --num_mlp_layers 1 --hidden_dim 200 --final_dropout .5 --configfile R8_save --num_heads 2
