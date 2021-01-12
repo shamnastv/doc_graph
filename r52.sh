5c7a62eaf155af0ee0ae089ac268f18af27ac329
Shamnas <shamnastv@gmail.com>
2021-01-12 11:22:54 +0530

    update

--- r52.sh
+++ r52.sh
@@ -5,4 +5,4 @@
 #SBATCH --output=test_job%j%x.out # Standard output and error log
 #SBATCH --gres=gpu:1
 #SBATCH --partition=cl1_48h-1G
-python3 main.py --lr .001 --num_layers 3 --num_mlp_layers 1 --hidden_dim 100 --learn_eps --configfile R52
+python3 main.py --lr .0001 --num_layers 3 --num_mlp_layers 1 --hidden_dim 200 --learn_eps --configfile R52 --num_heads 2
