e0b0403f1bc15ab5b10a492e6371a1bb63a27c62
Shamnas <shamnastv@gmail.com>
2021-01-12 15:24:48 +0530

    update

--- ohsumed.sh
+++ ohsumed.sh
@@ -5,4 +5,4 @@
 #SBATCH --output=test_job%j%x.out # Standard output and error log
 #SBATCH --gres=gpu:1
 #SBATCH --partition=cl1_48h-1G
-python3 main.py --lr .0001 --num_layers 2 --num_mlp_layers 1 --hidden_dim 200 --final_dropout .5 --learn_eps --configfile ohsumed --num_heads 2
+python3 main.py --lr .0001 --num_layers 3 --num_mlp_layers 1 --hidden_dim 200 --final_dropout .5 --learn_eps --configfile ohsumed --num_heads 2
