a8649ca09c4e98f0f811220617100bab70a82080
Shamnas <shamnastv@gmail.com>
2021-01-15 01:39:18 +0530

    update

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --partition=cl1_48h-1G
 
-python3 main.py --epochs 400 --lr .00005 --num_layers 3 --num_mlp_layers 1 --hidden_dim 200 --final_dropout .5 --configfile R8_save --num_heads 2
-python3 main.py --epochs 400 --lr .00008 --num_layers 3 --num_mlp_layers 1 --hidden_dim 200 --final_dropout .5 --configfile R8_save --num_heads 2
-python3 main.py --epochs 400 --lr .0001 --num_layers 3 --num_mlp_layers 1 --hidden_dim 200 --final_dropout .5 --configfile R8_save --num_heads 2
-python3 main.py --epochs 400 --lr .0003 --num_layers 3 --num_mlp_layers 1 --hidden_dim 200 --final_dropout .5 --configfile R8_save --num_heads 2
+python3 main.py --epochs 400 --lr .00005 --num_layers 3 --num_mlp_layers 1 --hidden_dim 200 --final_dropout .5 --configfile R52 --num_heads 2
+python3 main.py --epochs 400 --lr .00008 --num_layers 3 --num_mlp_layers 1 --hidden_dim 200 --final_dropout .5 --configfile R52 --num_heads 2
+python3 main.py --epochs 400 --lr .0001 --num_layers 3 --num_mlp_layers 1 --hidden_dim 200 --final_dropout .5 --configfile R52 --num_heads 2
+python3 main.py --epochs 400 --lr .0003 --num_layers 3 --num_mlp_layers 1 --hidden_dim 200 --final_dropout .5 --configfile R52 --num_heads 2
