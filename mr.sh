67717a3528fae115a5ce9dbb3e7958a40e1b1071
Shamnas <shamnastv@gmail.com>
2021-02-05 10:13:57 +0530

    update

--- mr.sh
+++ mr.sh
@@ -5,4 +5,6 @@
 #SBATCH --output=test_job%j%x.out # Standard output and error log
 #SBATCH --gres=gpu:1
 #SBATCH --partition=cl1_48h-1G
-python3 main.py --lr .0001 --num_layers 3 --num_mlp_layers 1 --hidden_dim 200 --final_dropout .5 --configfile mr --num_heads 2
+python3 main.py --lr .0001 --num_layers 3 --num_mlp_layers 1 --hidden_dim 100 --final_dropout .5 --configfile mr --num_heads 2
+python3 main.py --lr .0001 --num_layers 3 --num_mlp_layers 1 --hidden_dim 100 --final_dropout .5 --configfile mr --num_heads 4
+python3 main.py --lr .0001 --num_layers 3 --num_mlp_layers 1 --hidden_dim 50 --final_dropout .5 --configfile mr --num_heads 4
